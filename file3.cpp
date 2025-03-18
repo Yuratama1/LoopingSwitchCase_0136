23fb3c41f294279f1185ab34fd33b146f808b12d
Yuratama1 <yuratama661@gmail.com>
2025-03-18 20:12:13 +0700

    pembuatan menghitung luas persegi panjang

---
+++ file3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+float luasPersegiPanjang(float x, float y){
+    return x * y;
+}
+ float luasLingkaran(float x){
+    return 3.14 * x * x;
+ }
+
+ int main(){
+    int pilihan;
+    float panjang,lebar,jejari;
+    do{
+        cout << "===Menu===" << endl;
+        cout << "1. Menghitung Luas Persegi Panjang" << endl;
+        cout << "2. Menghitung Luas Lingkaran" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Pilih (1/2/3) : ";
+        cin >> pilihan;
+
+        switch(pilihan){
+            case 1 :
+                cout << "Menghitung Luas Persegi Panjang" << endl;
+                cout << "Masukkan Panjang = ";
+                cin >> panjang;
+                cout << "Masukkan Lebar = ";
+                cin >> lebar;
+                cout << "luas Persegi Panjang = " << luasPersegiPanjang(panjang,lebar) << endl;
+                break;
+            case 2 :
+                cout << "Menghitung Luas Lingkaran " << endl;
+            case 3 :
+                break;
+            default : 
+                cout << "Pilihan anda salah" << endl;
+                break;
+        }
+    }while(pilihan != 3);
+ }
